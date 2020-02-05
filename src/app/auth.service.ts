9b5ab2d10d39076d33c0e7ebdce42b9d1194e2b4
arnabbhat82 <arnabbhat@gmail.com>
2020-02-05 14:05:37 +0530

    service for facebook

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+  doFacebookLogin() {
+    return new Promise<any>((resolve, reject) => {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      this.afAuth.auth
+        .signInWithPopup(provider)
+        .then(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
 }
